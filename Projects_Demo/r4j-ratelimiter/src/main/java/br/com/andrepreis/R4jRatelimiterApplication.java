6e504463d23ccd5d31ae98e3932e46c886019fe2
apr1982 <apr1982@R490KBE55P1c7e544c7.ap-jose-henrique>
2022-12-03 22:02:32 -0300

    rate limiter

---
+++ Projects_Demo/r4j-ratelimiter/src/main/java/br/com/andrepreis/R4jRatelimiterApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrepreis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R4jRatelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R4jRatelimiterApplication.class, args);
+	}
+
+}
---
+++ Projects_Demo/r4j-ratelimiter/src/test/java/br/com/andrepreis/R4jRatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrepreis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R4jRatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
