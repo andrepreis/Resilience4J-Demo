19a279e82ec11a6e29e16fb675e4e62cad370047
Andre Reis <apr1982@gmail.com>
2022-12-06 12:36:36 -0300

    Ajustes no exemplo que simula lentidão na chamada de uma API

--- Projects_Demo/resilience4j-slow-calls/src/main/java/br/com/andrepreis/resilience4jslowcalls/controller/CircuitBrakerController.java
+++ Projects_Demo/resilience4j-slow-calls/src/main/java/br/com/andrepreis/resilience4jslowcalls/controller/CircuitBrakerController.java
@@ -1,23 +1,21 @@
 package br.com.andrepreis.resilience4jslowcalls.controller;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @CrossOrigin
@@ -26,6 +24,7 @@ public class CircuitBrakerController {
     
 	public static final Logger logger = LoggerFactory.getLogger(CircuitBrakerController.class);
 	public static String SUPERAPI = "http://localhost:9090/superapi/v1/getSlowCalls?name=";
+	Random random = new Random(-6732303926L);
 		
     @Autowired
     RestTemplate restTemplate;
@@ -33,49 +32,57 @@ public class CircuitBrakerController {
     private String cache = null;
 
     @GetMapping("/getSlowCalls")
-    @TimeLimiter(name = "timelimiterSlowMonitor", fallbackMethod = "fallBack")
-    @Bulkhead(name = "bulkheadMonitor", fallbackMethod = "fallBack", type = Bulkhead.Type.THREADPOOL)
-    public CompletableFuture<String> callApi(@RequestParam(value = "name", defaultValue = "bulkheadMonitor") String name) {
+    @CircuitBreaker(name = "circuitSlowMonitor", fallbackMethod = "fallBack")
+    public ResponseEntity<String> callApi(@RequestParam(value = "name", defaultValue = "circuitSlowMonitor") String name) {
             	    	
-    	ResponseEntity<String> responseEntity = restTemplate.getForEntity(SUPERAPI + name, String.class);
+    	ResponseEntity<String> responseEntity = restTemplate.getForEntity(SUPERAPI + random.nextInt(12), String.class);
+    	
+    	logger.info("Sucesso no processamento da requisição : "+SUPERAPI + name);
+    	logger.info(responseEntity.getBody().toString());
+    	logger.info("");
             	    
     	//Atualiza Cache
         cache = responseEntity.getBody().toString();
         
-        return CompletableFuture.completedFuture(cache);
+        return responseEntity;
        
     }
 
   
     /**
+     * Método invocado sempre que o circuito esta aberto
      * 
-     * @param name
-     * @param ex
-     * @return
+     * @param name : String enviada como parâmetro para a request
+     * @param ex : Exception lançada quando o circuito esta aberto
+     * 
+     * @return : Resposta alternativa para a requisição original
      */
-    private CompletableFuture<String> fallBack(String name, Exception ex) {
-
-        logger.info("Timeout na chamada do serviço " + SUPERAPI + name);
-
-        return CompletableFuture.completedFuture(cache);
+    public ResponseEntity<String>  fallBack(String name, io.github.resilience4j.circuitbreaker.CallNotPermittedException ex) {
+    	
+    	logger.info("<Circuitbreaker is open!> : "+SUPERAPI + name);
+    	logger.info("Valor retornado do cache : " + cache);
+    	logger.info("");
+        
+        //retornando último valor atualizado no cache
+        return ResponseEntity.ok().body(cache);
     }
 
     /**
+     * Método invocado sempre que a o serviço monitorado lançar alguma Exception
      * 
-     * @param name
-     * @param ex
-     * @return
+     * @param name : String enviada como parâmetro para a request
+     * @param ex :
+     * 
+     * @return : Resposta alternativa para a requisição original
      */
-    public ResponseEntity<String>  fallBack(String name, io.github.resilience4j.bulkhead.BulkheadFullException ex) {
-             
-    	logger.info("Bulkhead ativo. Nenhuma nova chamada será aceita");
-    	HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Retry-After", "10");
-
-        return ResponseEntity
-        		.status(HttpStatus.SERVICE_UNAVAILABLE)
-                .headers(responseHeaders)
-                .body("Muitas conexões simultâneas! Tente mais tarde");
+    public ResponseEntity <String> fallBack(String name, HttpServerErrorException ex) {
+        
+    	logger.info("<Erro a invocar Api!> : "+SUPERAPI + name);
+    	logger.info(cache);
+    	logger.info("");
+    	
+    	//retornando último valor atualizado no cache
+        return ResponseEntity.ok().body(cache);
     }
 
     @Bean
