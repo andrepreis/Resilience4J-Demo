1f7157018d0b3d06acc59cfc0d3ce5a5ad6a46ea
Andre Reis <apr1982@gmail.com>
2023-02-16 10:15:44 -0300

    Criação de repositório com exemplos do padrão de projeto Retry

---
+++ Projects_Demo/r4j-retry/src/main/java/br/com/andrepreis/resilience4jslowcalls/R4jRetryApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrepreis.resilience4jslowcalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R4jRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R4jRetryApplication.class, args);
+	}
+
+}
---
+++ Projects_Demo/r4j-retry/src/test/java/br/com/andrepreis/resilience4jslowcalls/R4jRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrepreis.resilience4jslowcalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R4jRetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
